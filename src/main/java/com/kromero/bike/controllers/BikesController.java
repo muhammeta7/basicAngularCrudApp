692e112197c7b1fb5b7838539b2aa27651186370
k-romero <imkevinromero@gmail.com>
2020-04-06 19:57:49 -0400

    creating controller

---
+++ src/main/java/com/kromero/bike/controllers/BikesController.java
@@ -0,0 +1,10 @@
+package com.kromero.bike.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/bikes")
+public class BikesController {
+
+}
